45c03b0971781fe0cc853ff0ed3d3f073480ad51
Kaique Cunha Mori <cunhakaique004@gmail.com>
2026-03-10 19:23:20 -0300

    Remove unnecessary blank line in ConsultAPI.java

--- VerificarCep/src/java/cep/ConsultAPI.java
+++ VerificarCep/src/java/cep/ConsultAPI.java
@@ -8,7 +8,6 @@ import java.net.http.HttpResponse;
 
 public class ConsultAPI {
 
-
     public String api (PutCep c) throws IOException, InterruptedException {
         try {
             String cep =  "https://"+ "viacep.com.br/ws/" + c.getCep() + "/json/";
